3a85ee2cb8f7a50a0b0b31d812801b2f41c45c3d
kaiki oliveira <103068974+kaiki95@users.noreply.github.com>
2022-11-09 22:30:30 -0300

    javascript

--- Portifólio/js/script.js
+++ Portifólio/js/script.js
@@ -1,33 +1,25 @@
 const header = document.querySelector( "header");
 window.addEventListener("scroll" , function(){
-    header.classList.toggle("sticky", window.scrollY > 0)
+  header.classList.toggle("sticky", window.scrollY > 0)
 
 })
 
-/*
+
 let menu = document.querySelector('#navbar-icon'); 
 let navlist = document.querySelector('.navbar-header');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x'); 
-    navlist.classList.toggle('active');
+  menu.classList.toggle('bx-x'); 
+  navlist.classList.toggle('active');
 }
-*/
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  
+
+
 const sr = ScrollReveal({
 
-    distance: '50px', 
-    duration: 2500, 
-    reset: true
+  distance: '50px', 
+  duration: 2500, 
+  reset: true
     
-    })
+})
     
-    sr.reveal('.home-text', {delay: 350, origin: 'left'})
+sr.reveal('.home-text', {delay: 350, origin: 'left'})
